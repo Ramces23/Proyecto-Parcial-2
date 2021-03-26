2eba91d48a344dee564464f46cb8e6934d74258c
ramces batista peña <ramcesbape@gmail.com>
2021-03-25 23:55:15 -0400

    Empleados fin

--- Empleados/Nomina.cs
+++ Empleados/Nomina.cs
@@ -15,21 +15,30 @@ namespace Empleados
         public int i;
         public void IntEmp()
         {
+            Console.Clear();
             Console.Write("Nombre: ");
             Empleado[i].Nombre = Console.ReadLine();
             Console.Write("\nSueldo: ");
             Empleado[i].Salario = float.Parse(Console.ReadLine());
+            Console.Clear();
         }
         public void Cargar() 
         {
             for (i = 0; i <= 4; i++)
             {
-                Console.WriteLine("Introdusca el nombre y el sueldo del empleado {0}", i++);
+                i++;
+                Console.WriteLine("Introdusca el nombre y el sueldo del empleado {0}", i);
+                i--;
+                IntEmp();
+
+
             }
         }
         public void SalarioMayor()
-        { 
-            for (int i = 0; i <= 4; i++)
+        {
+            int y = 0;
+            int c = 0;
+            for ( i = 0; i <= 4; i++)
             {
                 float p = Empleado[i].Salario;
                 int z = 0;
@@ -38,36 +47,102 @@ namespace Empleados
                     if (p - Empleado[e].Salario >= 0)
                     {
                         z++;
+                        if (p - Empleado[e].Salario == 0)
+                        {
+                            y++;
+                        }
+                    }
+
+                }
+          
+                if (y > 1 & c <= 0)
+                {
+                    if (z == 5 )
+                    {
+                        Console.WriteLine("Hay {0} empleados con el mismo salario, por lo cual los siguientes empleados tienen el salario mayor: ", y );
+ 
+                        for (int e = 0; e <= 4; e++)
+                        {
+                            if (p - Empleado[e].Salario == 0)
+                            {
+                                Console.WriteLine(Empleado[e].Nombre);
+                            }
+
+                        }
+                        Console.WriteLine("Tienen un salario de: " + Empleado[i].Salario);
+                        Console.ReadKey();
+                        Console.Clear();
+                        c++;
                     }
                 }
-
-                if (z == 10)
+                else
                 {
-                    Console.WriteLine("El mayor sueldo es: " + Empleado[i].Salario);
-
-                    Console.ReadKey();
-                    Console.Clear();
-
+                    if (z == 5 & y < 1)
+                    {
+                        Console.WriteLine("El empleado con el salario mayor es " + Empleado[i].Nombre);
+                        Console.WriteLine("Tiene un salario de: " + Empleado[i].Salario);
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
                 }
             }   
         }
         public void VerEmpleado() 
         {
-            Console.WriteLine("Nombre: {0}", Empleado[i].Nombre);
-            Console.WriteLine("Salario: {0}", Empleado[i].Salario);
+            if (i < 5)
+            {
+                Console.WriteLine("Nombre: {0}", Empleado[i].Nombre);
+                Console.WriteLine("Salario: {0}", Empleado[i].Salario);
+            }
 
         }
         public void BusEmpleado() 
         {
             String BusEmple = "";
-            Console.WriteLine("Escriba el nombre de su fichero: ");
-            BusEmple = Console.ReadLine();
-            i = -1;
+            int z = 0;
+
             do
             {
-                i++;
-            } while (BusEmple != Empleado[i].Nombre);
-            i++;
+                z = 0;
+                Console.WriteLine("Escriba el nombre de su fichero: ");
+                BusEmple = Console.ReadLine();
+                i = 0;
+                bool b;
+                int c = 0;
+
+
+                for (i = 0; i <= 4; i++)
+                {
+                    b = Empleado[i].Nombre.Contains(BusEmple);
+                    if (b = false)
+                    {
+                        c++;
+                    }
+                    b = Empleado[i].Nombre.Contains(BusEmple);
+                    if (b)
+                    {
+                        Console.WriteLine("ID de Empleado: {0}", i);
+                        VerEmpleado();
+                        c--;
+                    }
+                   else
+                    {
+                        if (z <= 0 )
+                        {
+                            if (c >= 5)
+                            {
+                                Console.WriteLine("No hay coincidencias ");
+                                z++;
+                            }
+                        }
+                    }
+                }
+
+            } while (z != 0);
+            Console.WriteLine("Digite el ID del empleado que quiere editar: ");
+            i = int.Parse(Console.ReadLine());
+            IntEmp();
+
           
         }
     }
--- Empleados/Program.cs
+++ Empleados/Program.cs
@@ -8,24 +8,57 @@ namespace Empleados
         {
             Nomina Nom = new Nomina();
             int a = 0;
-            Console.WriteLine(" ______________________________________________\n");
-            Console.WriteLine(" |                Administracion               |\n");
-            Console.WriteLine(" |_____________________________________________|\n");
-            Console.WriteLine(" | 1 - Introducir empleados                    |\n");
-            Console.WriteLine(" | 2 - cambiar los datos del empleado          |\n");
-            Console.WriteLine(" | 3 - Ver el empleado con el mayor salario    |\n");
-            Console.WriteLine(" |_____________________________________________|\n");
-            switch (a) 
+            do
             {
-                case 1:
-                    break;
-                case 2:
+                Console.WriteLine(" ______________________________________________\n");
+                Console.WriteLine(" |                Administracion               |\n");
+                Console.WriteLine(" |_____________________________________________|\n");
+                Console.WriteLine(" | 1 - Introducir empleados                    |\n");
+                Console.WriteLine(" | 2 - cambiar los datos del empleado          |\n");
+                Console.WriteLine(" | 3 - Ver el empleado con el mayor salario    |\n");
+                Console.WriteLine(" | 4 - Salir del programa                      |\n");
+                Console.WriteLine(" |_____________________________________________|\n");
+                a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Nom.Cargar();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        if (Nom.Empleado[0].Salario <= 0)
+                        {
+                            Console.WriteLine("Actualmente no sean introducido empleados, por favor introduzcalos ");
+                            Console.WriteLine("Presione enter para continuar ");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Nom.BusEmpleado();
+                        }
+                        break;
+                    case 3:
+                        Console.Clear();
+                        if (Nom.Empleado[0].Salario <= 0)
+                        {
+                            Console.WriteLine("Actualmente no sean introducido empleados, por favor introduzcalos ");
+                            Console.WriteLine("Presione enter para continuar ");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Nom.SalarioMayor();
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Gracias por usar este software ");
+                        Console.Clear();
+                        break;
 
-                    break;
-                case 3:
-                    break;
-
-            }
+                }
+            } while (a != 4);
 
         }
     }
